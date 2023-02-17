f5684e70fcd4ee7ff26fafff3ba582d3b774cf9e
Jesus Geovanni Labastida Vazquez <geo.labastidaa@gmail.com>
2023-02-16 19:50:34 -0700

    Layout v3

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -5,15 +5,32 @@ const Projects = () => {
         <div className='bg-black w-auto h-1000px' >
             <div className=' flex space-y-[14px] flex-col items-center justify-center'>
                 <p className=' text-white text-bold font-Inter font-bold text-[50px]'>Projects</p>
+                
                 <div className='flex items-center justify-center bg-white w-[94.9333333333%] h-[380px] rounded-[50px]'>
-                    <div className='grid grid-cols-3 grid-rows-3 bg-[#000]/50 w-[77.52809%] h-[300px]'>
-                        <p className=''>01.</p>
-                        <p className='col-start-1 row-start-3'>Fungi Projects</p>
-                        <span className='col-start-2 row-start-2 '>Icon</span>
+                    <div className='grid grid-cols-3 grid-rows-3 w-[77.52809%] h-[300px]'>
+                        <p className='text-[1.25rem] font-bold'>01.</p>
+                        <p className='flex col-span-2 row-start-3 items-end font-bold text-[1.25rem]'>Fungi Projects</p>
+                        <span className='flex col-start-2 row-start-2 items-center justify-center'>Icon</span>
                     </div>
                 </div>
-                <div className='bg-custLightGreen w-[94.9333333333%] h-[380px] rounded-[50px]'></div>
-                <div className='bg-custDarkGreen w-[94.9333333333%] h-[380px] rounded-[50px]'></div>
+                <div className='flex items-center justify-center bg-custLightGreen w-[94.9333333333%] h-[380px] rounded-[50px]'>
+                    <div className='grid grid-cols-3 grid-rows-3 w-[77.52809%] h-[300px]'>
+                        <p className='text-[1.25rem] font-bold'>02.</p>
+                        <p className='flex col-span-3 row-start-3 items-end font-bold text-[1.25rem]'>Permaculture Projects</p>
+                        <span className='flex col-start-2 row-start-2 items-center justify-center'>
+                            <img className='' src="/src/assets/plant.svg" alt="" />
+                        </span>
+                    </div>
+                </div>
+                <div className='flex items-center justify-center bg-custDarkGreen w-[94.9333333333%] h-[380px] rounded-[50px]'>
+                    <div className='grid grid-cols-3 grid-rows-3 w-[77.52809%] h-[300px]'>
+                        <p className='text-[1.25rem] font-bold text-[#98F0E2]' >03.</p>
+                        <p className='flex col-span-2 row-start-3 items-end font-bold text-[1.25rem] text-[#98F0E2]'>Fungi Projects</p>
+                        <span className='flex col-start-2 row-start-2 items-center justify-center width-[10px]'>
+                            <img className='' src="/src/assets/fungi5.svg" alt="" />
+                        </span>
+                    </div>
+                </div>
             </div>
         </div>
     )
--- src/Components/Technologies.jsx
+++ src/Components/Technologies.jsx
@@ -10,22 +10,22 @@ const Navbar = () => {
 Focusing on using best practices to maintain clean code and more </p>
 
                 <div className=' '>
-                    <div className='bg-[#EBD907] w-screen h-[390px] rounded-[50px] flex items-center justify-center text-[50px] font-bold text-black' >
+                    <div className='bg-[#EBD907] w-screen h-[390px] rounded-[50px] flex items-center justify-center text-[50px] font-bold text-black sticky top-[108px] z-0' >
                         JavaScript
                     </div>
-                    <div className='bg-[#4158AB] w-screen h-[390px] rounded-[50px] flex items-center justify-center text-[50px] font-bold text-black' >
+                    <div className='bg-[#4158AB] w-screen h-[390px] rounded-[50px] flex items-center justify-center text-[50px] font-bold text-black sticky top-[148px] z-1' >
                         CSS
                     </div>
-                    <div className='bg-[#B64F2F] w-screen h-[390px] rounded-[50px] flex items-center justify-center text-[50px] font-bold text-black' >
+                    <div className='bg-[#B64F2F] w-screen h-[390px] rounded-[50px] flex items-center justify-center text-[50px] font-bold text-black sticky top-[188px] z-2' >
                         HTML
                     </div>
-                    <div className='bg-[#EBD907] w-screen h-[390px] rounded-[50px] flex items-center justify-center text-[50px] font-bold text-black' >
+                    <div className='bg-[#411102] w-screen h-[390px] rounded-[50px] flex items-center justify-center text-[50px] font-bold text-black sticky top-[228px] z-3' >
                         Tailwind
                     </div>
-                    <div className='bg-[#EBD907] w-screen h-[390px] rounded-[50px] flex items-center justify-center text-[50px] font-bold text-black' >
+                    <div className='bg-[#D8D8D8] w-screen h-[390px] rounded-[50px] flex items-center justify-center text-[50px] font-bold text-black sticky top-[268px] z-4' >
                         Npm
                     </div>
-                    <div className='bg-[#EBD907] w-screen h-[390px] rounded-[50px] flex items-center justify-center text-[50px] font-bold text-black' >
+                    <div className='bg-[#BEA2BE] w-screen h-[390px] rounded-[50px] flex items-center justify-center text-[50px] font-bold text-black sticky top-[308px] z-5' >
                         React
                     </div>
                 </div>
